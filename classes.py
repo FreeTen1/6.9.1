3176e520465bd23df3dfc66eece6c912ad1b74e3
Misha <mishatambov@gmail.com>
2020-11-03 22:38:49 +0300

    add_circle_class

--- classes.py
+++ classes.py
@@ -12,3 +12,19 @@ class Cat:
 
     def getAge(self):
         return self.age
+
+
+class Circle:
+    pi = 3.14
+
+    def __init__(self, r):
+        self.r = r
+
+    def setRadius(self, r):
+        self.r = r
+
+    def getRadius(self):
+        return self.r
+
+    def get_area_circle(self):
+        return self.pi * self.r ** 2
