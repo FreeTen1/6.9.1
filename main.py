d24d480051324724177f277c7942f719f00006f8
Misha <mishatambov@gmail.com>
2020-11-03 22:39:14 +0300

    change_main

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from classes import Cat
+from classes import Cat, Circle
 
 cats = [{"name": "Барон", "sex": "мальчик", "age": 2}, {"name": "Сэм", "sex": "мальчик", "age": 2}]
 
 for cat in cats:
     obj_cat = Cat(name=cat.get("name"), sex=cat.get("sex"), age=cat.get("age"))
     print(obj_cat.getName(), obj_cat.getSex(), obj_cat.getAge())
+
+c1 = Circle(5)
+print("\nПлощадь круга с радиусом", c1.getRadius(), "=", c1.get_area_circle())
+c1.setRadius(8)
+print("\nПлощадь круга с радиусом", c1.getRadius(), "=", c1.get_area_circle())
+
